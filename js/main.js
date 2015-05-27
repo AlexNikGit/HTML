a63fffb1a0470dc1f85ca272851f956b40dbb0cd
Alex_Nik <alexnikmail@gmail.com>
2015-05-27 12:07:11 +0300

    Область входа с анимацией, главные панели/меню раздвигаются при помощи всплывающих кнопок, виджет входа в систему

--- js/main.js
+++ js/main.js
@@ -8,8 +8,32 @@ function procEnterClick( ) {
       document.getElementById( "wdlEnter" ).style.display = "none";
       document.getElementById( "wdlLogin" ).style.display = "block";
    }, 1500);
-};
+}
 
-function procMenuHeader( ) { document.getElementById( "wdlMenuHeader").classList.toggle( "active" ); }
-function procPanelLeft( ) { document.getElementById( "wdlPanelLeft").classList.toggle( "active" ); }
-function procPanelRight( ) { document.getElementById( "wdlPanelRight").classList.toggle( "active" ); }
+function procMenuHeader( ) {
+   document.getElementById( "wdlMenuHeader").classList.toggle( "active" );
+   setTimeout( function( ) {
+      var objStyle = document.getElementById( "btnMenuHeader" ).style;
+      if( objStyle.backgroundImage )
+         objStyle.backgroundImage = null;
+      else objStyle.backgroundImage = "url('ico/panel_down.svg')";
+   }, 300);      /* равно времени, установленному на анимацию в таблице стилей */
+}
+function procPanelLeft( ) {
+   document.getElementById( "wdlPanelLeft").classList.toggle( "active" );
+   setTimeout( function( ) {
+      var objStyle = document.getElementById( "btnPanelLeft" ).style;
+      if( objStyle.backgroundImage )
+         objStyle.backgroundImage = null;
+      else objStyle.backgroundImage = "url('ico/left_open.svg')";
+   }, 300);      /* равно времени, установленному на анимацию в таблице стилей */
+}
+function procPanelRight( ) {
+   document.getElementById( "wdlPanelRight").classList.toggle( "active" );
+   setTimeout( function( ) {
+      var objStyle = document.getElementById( "btnPanelRight" ).style;
+      if( objStyle.backgroundImage )
+         objStyle.backgroundImage = null;
+      else objStyle.backgroundImage = "url('ico/right_open.svg')";
+   }, 300);      /* равно времени, установленному на анимацию в таблице стилей */
+}
