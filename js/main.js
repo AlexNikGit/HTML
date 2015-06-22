220263de1c39f51f7f44dcccf41cde99796c6658
Alex_Nik <alexnikmail@gmail.com>
2015-06-22 12:58:26 +0300

    Добавление SVG-стрелочек анимации при наведении на кнопку входа

--- js/main.js
+++ js/main.js
@@ -9,6 +9,70 @@ function procEnterClick( ) {
       document.getElementById( "wdlLogin" ).style.display = "block";
    }, 1500);
 }
+var arrowAnimationStep = 0;
+var arrowAnimationTimer;
+function doAnimateArrow( ) {     /* переделать на анимацию в CSS через добавочные стили, типа active и раздвижение в стороны вместе с разделительными частями экрана */
+   if( arrowAnimationStep == 1 ) {
+      document.getElementById("arrTL1").setAttribute("fill", "#fafafa");
+      document.getElementById("arrBL1").setAttribute("fill", "#e1e1e1");
+      document.getElementById("arrTR1").setAttribute("fill", "#fafafa");
+      document.getElementById("arrBR1").setAttribute("fill", "#e1e1e1");
+      document.getElementById("arrTL3").setAttribute("fill", "#fafafa");
+      document.getElementById("arrBL3").setAttribute("fill", "#e1e1e1");
+      document.getElementById("arrTR3").setAttribute("fill", "#fafafa");
+      document.getElementById("arrBR3").setAttribute("fill", "#e1e1e1");
+      document.getElementById("arrTL5").setAttribute("fill", "#fafafa");
+      document.getElementById("arrBL5").setAttribute("fill", "#e1e1e1");
+      document.getElementById("arrTR5").setAttribute("fill", "#fafafa");
+      document.getElementById("arrBR5").setAttribute("fill", "#e1e1e1");
+
+      document.getElementById("arrTL2").setAttribute("fill", "#afafaf");
+      document.getElementById("arrBL2").setAttribute("fill", "#969696");
+      document.getElementById("arrTR2").setAttribute("fill", "#afafaf");
+      document.getElementById("arrBR2").setAttribute("fill", "#969696");
+      document.getElementById("arrTL4").setAttribute("fill", "#afafaf");
+      document.getElementById("arrBL4").setAttribute("fill", "#969696");
+      document.getElementById("arrTR4").setAttribute("fill", "#afafaf");
+      document.getElementById("arrBR4").setAttribute("fill", "#969696");
+      arrowAnimationStep = 2;
+   } else if( arrowAnimationStep == 2 ) {
+      document.getElementById("arrTL1").setAttribute("fill", "#afafaf");
+      document.getElementById("arrBL1").setAttribute("fill", "#969696");
+      document.getElementById("arrTR1").setAttribute("fill", "#afafaf");
+      document.getElementById("arrBR1").setAttribute("fill", "#969696");
+      document.getElementById("arrTL3").setAttribute("fill", "#afafaf");
+      document.getElementById("arrBL3").setAttribute("fill", "#969696");
+      document.getElementById("arrTR3").setAttribute("fill", "#afafaf");
+      document.getElementById("arrBR3").setAttribute("fill", "#969696");
+      document.getElementById("arrTL5").setAttribute("fill", "#afafaf");
+      document.getElementById("arrBL5").setAttribute("fill", "#969696");
+      document.getElementById("arrTR5").setAttribute("fill", "#afafaf");
+      document.getElementById("arrBR5").setAttribute("fill", "#969696");
+
+      document.getElementById("arrTL2").setAttribute("fill", "#fafafa");
+      document.getElementById("arrBL2").setAttribute("fill", "#e1e1e1");
+      document.getElementById("arrTR2").setAttribute("fill", "#fafafa");
+      document.getElementById("arrBR2").setAttribute("fill", "#e1e1e1");
+      document.getElementById("arrTL4").setAttribute("fill", "#fafafa");
+      document.getElementById("arrBL4").setAttribute("fill", "#e1e1e1");
+      document.getElementById("arrTR4").setAttribute("fill", "#fafafa");
+      document.getElementById("arrBR4").setAttribute("fill", "#e1e1e1");
+      arrowAnimationStep = 1;
+   }
+   arrowAnimationTimer = setTimeout(doAnimateArrow, 500);
+}
+function procEnterMFocus( ) {
+   document.getElementById( "wdgArrowBarLeft").classList.toggle( "active" );
+   document.getElementById( "wdgArrowBarRight").classList.toggle( "active" );
+   if( !arrowAnimationStep ) {
+      arrowAnimationStep = 1;
+      doAnimateArrow( );
+   }
+   else {
+      arrowAnimationStep = 0;
+      clearTimeout( arrowAnimationTimer );
+   }
+}
 
 function procMenuHeader( ) {
    document.getElementById( "wdlMenuHeader").classList.toggle( "active" );
